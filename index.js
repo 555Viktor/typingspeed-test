cf7f09b021666bd1a5da8d8c0f7c3a59d3374f36
Viktor <victor.vaskov2002@gmail.com>
2024-09-17 11:06:13 +0300

    Added restart game functionality + dynamic scroll

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Example sentences
+// Example words
 const wordsArray = [
     "abacus", "butterfly", "cactus", "dolphin", "eagle",
     "fountain", "grape", "horizon", "iguana", "jigsaw",
@@ -15,8 +15,9 @@ const wordsArray = [
 
 //DOM elements
 const textDisplay = document.querySelector('.text-display p');
-const timerDisplay = document.querySelector('#timer-display')
-const userInputField = document.querySelector('.input-wrapper input')
+const timerDisplayHeader = document.querySelector('#timer-display-header');
+const timerDisplay = document.querySelector('#timer-display');
+const userInputField = document.querySelector('.input-wrapper input');
 
 const btnRestart = document.querySelector('#btn-restart');
 
@@ -27,10 +28,9 @@ const spanTotalWords = document.querySelector('.totalWords span');
 const spanAvgWps = document.querySelector('.averageWps span');
 const spanMistakes = document.querySelector('.mistakes span');
 
-
 // Result variables 
 let totalClicks = 0;
-let clicksPerSecond = totalClicks;
+let clicksPerSecond;
 let totalWords = 0;
 let averageWps;
 let mistakes = 0;
@@ -39,29 +39,38 @@ spanTotalWords.textContent = totalWords;
 spanAvgWps.textContent = averageWps;
 spanMistakes.textContent = mistakes;
 
+// Falsy variables
 let charIndex = 0;
 let isTyping = false;
 let timerRunning = false;
 
 // Timer variables
 let timer;
-
 let maxTime = 30;
 let timeLeftMs = maxTime * 1000;
 
-
+// Functions timer
 function startTimer () {
     timer = setInterval(displayTimer, 10);
+    timerRunning = true;
+}
+
+function stopTimer () {
+    clearInterval(timer);
+    timeLeftMs = 0;
 }
 
 function displayTimer () {
     timeLeftMs -= 10;
 
-    if (timeLeftMs == 0) {
-        clearInterval(timer);
-        timeLeftMs = 0;
-    };
+    if (timeLeftMs <= 0) {
+        stopTimer();
+        timerDisplay.innerHTML = `00:${maxTime}:00`
+        userInputField.disabled = true;
     
+        timerDisplayHeader.innerHTML = 'Time over!';
+    }
+
     let milliseconds = timeLeftMs % 1000;
     let seconds = Math.floor((timeLeftMs % (1000 * 60)) / 1000);
     let minutes = Math.floor((timeLeftMs % (1000 * 60 * 60)) / (1000 * 60));
@@ -79,6 +88,7 @@ function displayTimer () {
     timerDisplay.innerHTML = `${min}:${sec}:${ms}`;
 }
 
+// Functions text && user input
 
 function loadText () {
     textDisplay.innerHTML = '';
@@ -121,15 +131,14 @@ function checkUserInput () {
 
 }
 
+// Start / Restart game 
 function startGame () {
-
     if (!isTyping) {
         isTyping = true;
     }
 
     if (!timerRunning) {
         startTimer();
-        timerRunning = true;
     }
 
     let chars = textDisplay.querySelectorAll('span');
@@ -140,16 +149,56 @@ function startGame () {
     chars.forEach(span => span.classList.remove("active"));
     chars[charIndex].classList.add("active");
 
-    checkUserInput(); 
+    checkUserInput();
 }
 
+function clearResults () {
+    let spanArrayResults = document.querySelectorAll('.result-container span');
 
+    totalClicks = 0;
+    clicksPerSecond = totalClicks;
+    totalWords = 0;
+    averageWps;
+    mistakes = 0;
 
+    spanArrayResults.forEach(span => {
+        span.innerHTML = 0;
+    })
+}
+
+function restartGame () {
+    stopTimer();
+    timeLeftMs = maxTime * 1000;
+    charIndex = 0;
+    isTyping = false;
+    timerRunning = false;
+    timerDisplay.innerHTML = `00:${maxTime < 10 ? '0' : ''}${maxTime}:00`;
+    clearResults();
+    textDisplay.scrollLeft = 0;
+    userInputField.value = '';
+    loadText();
+    userInputField.disabled = false;
+}
+
+// Init
 loadText()
-timerDisplay.innerHTML = `00:30:00`;
-
+timerDisplay.innerHTML = `00:${maxTime < 10 ? '0' : ''}${maxTime}:00`;
 userInputField.addEventListener('input', () => startGame());
+
 userInputField.addEventListener('keyup', () => {
+    if (!timerRunning) return
     totalClicks++;
     spanTotalClicks.textContent = totalClicks;
+    clicksPerSecond = totalClicks / maxTime;
+    spanCps.textContent = clicksPerSecond.toFixed(2);
 })
+
+btnRestart.addEventListener('click', restartGame);
+
+userInputField.addEventListener('keyup', (event) => {
+    if (event.key === 'Backspace') {
+        textDisplay.scrollLeft -= 6;
+    } else {
+        textDisplay.scrollLeft += 6;
+    }
+});
